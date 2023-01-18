d2a1e9285c3969df0fb9be87cff925c8a579095f
i_badro <ilnurb@mail.ru>
2023-01-18 19:03:57 +0500

    !

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите любок количество кр.скобок");
+        System.out.println("Введите любок количество кр. скобок");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         String output = StrHelp.getCountAndValidParent(input);
--- src/util/StrHelp.java
+++ src/util/StrHelp.java
@@ -6,40 +6,41 @@ import java.util.List;
 import java.util.Stack;
 
 public class StrHelp {
-    private static final Character OPEN_PARENT = '(';
-    private static final Character CLOSE_PARENT = ')';
-    private static final String REPLACING_PARENTHESIS_REGEXP = "[^\\(\\)]";
-    private static String ZERO_RESPONSE = "0";
+    private static final Character OPEN = '(';
+    private static final Character CLOSE = ')';
+    private static final String REPL_PARENT_REGEXP = "[^\\(\\)]";
+    private static String ZERO_RESP = "0";
 
     public static String getCountAndValidParent(String input) {
-        Stack<AbstractMap.SimpleEntry<Integer, Character>> validationStack = new Stack<>();
+        Stack<AbstractMap.SimpleEntry<Integer, Character>> validStack = new Stack<>();
         Stack<AbstractMap.SimpleEntry<Integer, Character>> charsWithIndexesStack = new Stack<>();
         StringBuilder stringBuilder = new StringBuilder();
 
         //Валидация входных символов
-        input = input.replaceAll(REPLACING_PARENTHESIS_REGEXP, "");
+        input = input.replaceAll(REPL_PARENT_REGEXP, "");
         if (input.isEmpty()) {
-            return ZERO_RESPONSE;
+            return ZERO_RESP;
         }
 
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
-            if (c == OPEN_PARENT && i != input.length() - 1) {
+
+            if (c == OPEN && i != input.length() - 1) {
                 charsWithIndexesStack.push(new AbstractMap.SimpleEntry<>(i, c));
-                validationStack.push(new AbstractMap.SimpleEntry<>(i, c));
-            } else if (c == CLOSE_PARENT && !validationStack.isEmpty()) {
-                validationStack.pop();
+                validStack.push(new AbstractMap.SimpleEntry<>(i, c));
+            } else if (c == CLOSE && !validStack.isEmpty()) {
+                validStack.pop();
                 charsWithIndexesStack.push(new AbstractMap.SimpleEntry<>(i, c));
             }
         }
 
-        //Исключение открывающих скобок, для которых не нашлось закрывающих
-        List<Integer> exceptionIndexes = new ArrayList<>();
-        if (!validationStack.isEmpty()) {
-            validationStack
+        //Исключение непарных скобок
+        List<Integer> exceptIndex = new ArrayList<>();
+        if (!validStack.isEmpty()) {
+            validStack
                     .elements()
                     .asIterator()
-                    .forEachRemaining(el -> exceptionIndexes.add(el.getKey()));
+                    .forEachRemaining(el -> exceptIndex.add(el.getKey()));
         }
 
         //Сборка итоговой строки валидных пар скобок.
@@ -47,13 +48,13 @@ public class StrHelp {
                 .elements()
                 .asIterator()
                 .forEachRemaining(elem -> {
-                    if (!exceptionIndexes.contains(elem.getKey())) {
+                    if (!exceptIndex.contains(elem.getKey())) {
                         stringBuilder.append(elem.getValue());
                     }
                 });
 
-        String validParentheses = stringBuilder.toString();
-        return !validParentheses.isEmpty() ? validParentheses.length() + " - " + validParentheses : ZERO_RESPONSE;
+        String validParent = stringBuilder.toString();
+        return !validParent.isEmpty() ? validParent.length() + " - " + validParent : ZERO_RESP;
     }
 }
 
